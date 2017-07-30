c20e2dfeda546aab8a677f8b65ed096797846a8e
mohammad <md.abdous@gmail.com>
2017-07-31 00:57:25 +0430

    raw triple extractor

--- src/main/java/ir/ac/iust/dml/kg/raw/services/access/repositories/RuleRepository.java
+++ src/main/java/ir/ac/iust/dml/kg/raw/services/access/repositories/RuleRepository.java
@@ -3,7 +3,11 @@ package ir.ac.iust.dml.kg.raw.services.access.repositories;
 import ir.ac.iust.dml.kg.raw.services.access.entities.Rule;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface RuleRepository extends MongoRepository<Rule, ObjectId> {
-
+    @Query("{ find: {} }")
+    List<Rule> getAllRules();
 }
