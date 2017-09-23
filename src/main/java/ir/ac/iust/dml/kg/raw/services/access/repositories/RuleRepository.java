ebb8bfc538f4b7a98d339ec30aa3463e4a37bd3e
Majid Asgari <majid.asgari@gmail.com>
2017-09-23 15:52:29 +0330

    Adding copyright header to java files

--- src/main/java/ir/ac/iust/dml/kg/raw/services/access/repositories/RuleRepository.java
+++ src/main/java/ir/ac/iust/dml/kg/raw/services/access/repositories/RuleRepository.java
@@ -1,3 +1,9 @@
+/*
+ * Farsi Knowledge Graph Project
+ *  Iran University of Science and Technology (Year 2017)
+ *  Developed by Majid Asgari.
+ */
+
 package ir.ac.iust.dml.kg.raw.services.access.repositories;
 
 import ir.ac.iust.dml.kg.raw.services.access.entities.Rule;
@@ -8,6 +14,6 @@ import org.springframework.data.mongodb.repository.Query;
 import java.util.List;
 
 public interface RuleRepository extends MongoRepository<Rule, ObjectId> {
-    @Query("{ find: {} }")
-    List<Rule> getAllRules();
+  @Query("{ find: {} }")
+  List<Rule> getAllRules();
 }
